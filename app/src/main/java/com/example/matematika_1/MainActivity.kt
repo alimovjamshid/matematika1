09e6268460c73c7c0544bc7a7bd56cd5eea862e0
alimovjamshid <jamshidalimov3464@gmail.com>
2022-08-14 00:54:22 +0500

    matematika 1

--- app/src/main/java/com/example/matematika_1/MainActivity.kt
+++ app/src/main/java/com/example/matematika_1/MainActivity.kt
@@ -1,21 +1,26 @@
 package com.example.matematika_1
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.ColorFilter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        val btn=findViewById<Button>(R.id.button)
+        val btn=findViewById<ImageView>(R.id.play)
         btn.setOnClickListener(View.OnClickListener {
+            btn.setColorFilter(Color.GRAY)
             val intent=Intent(this,PlayActivity::class.java)
             startActivity(intent)
         })
